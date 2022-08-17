846ad6d3b32252cb7980ce2e65a60af538b1b366
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-08-17 03:10:43 +0200

    I am back! checking out Image component

--- components/layout/Sidebar/Sidebar.jsx
+++ components/layout/Sidebar/Sidebar.jsx
@@ -1,7 +1,16 @@
 import { StyledDiv } from './sidebar.styles';
+import Image from 'next/image';
 
 const Sidebar = () => {
-  return <StyledDiv className='sidebar'>Sidebar</StyledDiv>;
+  return (
+    <StyledDiv className='sidebar'>
+      <Image
+        src='https://www.yellow.com.mt/sys/img/1043/12021/metadata/275c8b7b-e0f1-4700-a701-0ee96660ad30.jpg_P.jpg'
+        width={50}
+        height={50}
+      />
+    </StyledDiv>
+  );
 };
 
 export default Sidebar;
